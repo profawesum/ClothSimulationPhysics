2d4f2a09bc76c1aff3c4ceeddac24984e1e73c95
prof <profawesum@Gmail.com>
2019-10-22 14:47:48 +1300

    Made particle into its own cpp and header file

---
+++ ClothSimulation/ClothSimulation/Particle.cpp
@@ -0,0 +1,16 @@
+#include "Particle.h"
+#include "glm.hpp"
+
+float Damping = 0.1f;
+float TimeStepSize = 0.5*0.5;
+float ConstraintIterations = 25;
+
+void Particle::timeStep() {
+	if (movable) {
+		vec3 temp = pos;
+		pos = pos + (pos - oldPos) * (1.0f - Damping) + acceleration * TimeStepSize;
+		oldPos = temp;
+		acceleration = vec3(0, 0, 0);
+	}
+}
+
--- ClothSimulation/ClothSimulation/Particle.h
+++ ClothSimulation/ClothSimulation/Particle.h
@@ -4,60 +4,45 @@
 #include <math.h>
 #include <vector>
 #include <iostream>
+#include "glm.hpp"
 
-//local includes
-#include "Vec3.h"
+using namespace glm;
 
-/* The particle class represents a particle of mass that can move around in 3D space*/
+extern float Damping;
+extern float TimeStepSize;
+extern float ConstraintIterations;
+
 class Particle
 {
-private:
-	bool movable; // can the particle move or not ? used to pin parts of the cloth
-
-	float mass; // the mass of the particle (is always 1 in this example)
-	Vec3 pos; // the current position of the particle in 3D space
-	Vec3 old_pos; // the position of the particle in the previous time step, used as part of the verlet numerical integration scheme
-	Vec3 acceleration; // a vector representing the current acceleration of the particle
-	Vec3 accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
 
 public:
-	Particle(Vec3 pos) : pos(pos), old_pos(pos), acceleration(Vec3(0, 0, 0)), mass(1), movable(true), accumulated_normal(Vec3(0, 0, 0)) {}
+
+	bool movable;
+	float mass;
+
+	vec3 pos;
+	vec3 oldPos;
+	vec3 acceleration;
+	vec3 accumulatedNormal;
+
+	//constructor/destructor
+	Particle(vec3 pos) : pos(pos), oldPos(pos), acceleration(vec3(0, 0, 0)), mass(1), movable(true), accumulatedNormal(vec3(0, 0, 0)) {}
 	Particle() {}
 
-	void addForce(Vec3 f)
-	{
-		acceleration += f / mass;
-	}
+	void addForce(vec3 f) { acceleration += f / mass; }
 
-	/* This is one of the important methods, where the time is progressed a single step size (TIME_STEPSIZE)
-	   The method is called by Cloth.time_step()
-	   Given the equation "force = mass * acceleration" the next position is found through verlet integration*/
-	void timeStep()
-	{
-		if (movable)
-		{
-			Vec3 temp = pos;
-			pos = pos + (pos - old_pos)*(1.0 - DAMPING) + acceleration * TIME_STEPSIZE2;
-			old_pos = temp;
-			acceleration = Vec3(0, 0, 0); // acceleration is reset since it HAS been translated into a change in position (and implicitely into velocity)	
-		}
-	}
+	void resetAcceleration() { acceleration = vec3(0, 0, 0); }
 
-	Vec3& getPos() { return pos; }
-
-	void resetAcceleration() { acceleration = Vec3(0, 0, 0); }
-
-	void offsetPos(const Vec3 v) { if (movable) pos += v; }
+	void offsetPos(const vec3 v) { if (movable) pos += v; }
 
 	void makeUnmovable() { movable = false; }
 
-	void addToNormal(Vec3 normal)
-	{
-		accumulated_normal += normal.normalized();
-	}
+	void addToNormal(vec3 normal) { accumulatedNormal += normalize(normal); }
 
-	Vec3& getNormal() { return accumulated_normal; } // notice, the normal is not unit length
+	vec3 getNormal() { return accumulatedNormal; }
+	
+	void resetNormal() { accumulatedNormal = vec3(0, 0, 0); }
 
-	void resetNormal() { accumulated_normal = Vec3(0, 0, 0); }
+	void timeStep();
 
 };
